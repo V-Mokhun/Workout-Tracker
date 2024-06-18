f469770883c770080267ed492089a65713351c71
V-Mokhun <mohunmax1408@gmail.com>
2024-06-18 15:18:54 +0200

    feat: upload indicator on image hover

--- src/app/(dashboard)/dashboard/account/_ui/account-image-upload.tsx
+++ src/app/(dashboard)/dashboard/account/_ui/account-image-upload.tsx
@@ -5,6 +5,7 @@ import { useToast } from "@/shared/ui";
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { deleteOldAvatar, updateAccountAvatar } from "./actions";
+import { CloudUploadIcon } from "lucide-react";
 
 interface AccountImageUploadProps {
   avatar: string;
@@ -61,7 +62,11 @@ export const AccountImageUpload = ({ avatar }: AccountImageUploadProps) => {
     >
       {({ open, isLoading }) => {
         return (
-          <button onClick={() => open()} disabled={isLoading}>
+          <button
+            className="group relative block rounded-full overflow-hidden after:absolute after:w-full after:h-full after:inset-0 after:opacity-0 after:bg-white/80 after:transition-opacity hover:after:opacity-100"
+            onClick={() => open()}
+            disabled={isLoading}
+          >
             <Image
               className={cn(
                 "rounded-full object-cover aspect-square overflow-hidden shrink-0",
@@ -72,6 +77,7 @@ export const AccountImageUpload = ({ avatar }: AccountImageUploadProps) => {
               width={128}
               height={128}
             />
+            <CloudUploadIcon className="group-hover:opacity-100 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-12 h-12 opacity-0 transition-opacity z-[1]" />
           </button>
         );
       }}
